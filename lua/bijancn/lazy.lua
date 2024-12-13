54700e4c047d8b9c180a5a8bb1c19bc7876c3881
Bijan Chokoufe Nejad <github@bijan.cloud>
2024-12-13 10:19:58 +0100

    Back to script

--- lua/bijancn/lazy.lua
+++ lua/bijancn/lazy.lua
@@ -347,8 +347,8 @@ require("lazy").setup({
             provider = "openai",
             openai = {
                 model = "gpt-4o",
-                api_key_name = "cmd:gopass show -o websites/openai.com/openai.com@bijan.cloud"
-                -- api_key_name = "cmd:sh /Users/bcn/repos/bcn_scripts/bin/get-openapi-key"
+                -- api_key_name = "cmd:gopass show -o websites/openai.com/openai.com@bijan.cloud"
+                api_key_name = "cmd:sh /Users/bcn/repos/bcn_scripts/bin/get-openapi-key"
             }
         },
         dependencies = {
