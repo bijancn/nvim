84beaa713c4d7038c60561d50175d96934895f2f
Bijan Chokoufe Nejad <bijan.chokoufe@jimdo.com>
2025-09-04 12:48:14 +0200

    remove autocompletes

--- lua/bijancn/lazy.lua
+++ lua/bijancn/lazy.lua
@@ -255,56 +255,6 @@ require("lazy").setup({
             }
         },
 
-        {
-            "zbirenbaum/copilot.lua",
-            cmd = "Copilot",
-            event = "InsertEnter",
-            lazy = true,
-            config = function()
-                require("copilot").setup({
-                    panel = { enabled = false },
-                    suggestion = {
-                        enabled = false,
-                        auto_trigger = true,
-                        keymap = {
-                            accept = "<M-j>",
-                            accept_word = false,
-                            accept_line = false,
-                            next = "<M-]>",
-                            prev = "<M-[>",
-                            dismiss = "<C-]>",
-                        },
-                    },
-                    keymap = {
-                        jump_prev = "[[",
-                        jump_next = "]]",
-                        accept = "<Tab>",
-                        refresh = "gr",
-                        open = "<M-CR>"
-                    },
-                    filetypes = {
-                        yaml = true,
-                        markdown = false,
-                        norg = false,
-                        help = false,
-                        gitcommit = true,
-                        gitrebase = true,
-                        ["."] = false,
-                    }
-                })
-            end,
-        },
-        {
-            "zbirenbaum/copilot-cmp",
-            dependencies = {
-                "zbirenbaum/copilot.lua",
-            },
-            config = function()
-                require("copilot_cmp").setup()
-            end
-        },
-
-
         { "AndrewRadev/linediff.vim" },
 
         { "numToStr/Comment.nvim" },
@@ -340,7 +290,7 @@ require("lazy").setup({
         { "rose-pine/neovim",     name = "rose-pine" },
         {
             "f-person/auto-dark-mode.nvim",
-            opts = {
+            config = {
                 update_interval = 1000,
                 set_dark_mode = function()
                     vim.api.nvim_set_option("background", "dark")
@@ -361,51 +311,6 @@ require("lazy").setup({
             end,
             opts = {
             }
-        },
-        {
-            "yetone/avante.nvim",
-            event = "VeryLazy",
-            lazy = false,
-            version = false, -- set this if you want to always pull the latest change
-            opts = {
-                -- add any opts here
-            },
-            -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-            build = "make",
-            dependencies = {
-                "stevearc/dressing.nvim",
-                "nvim-lua/plenary.nvim",
-                "MunifTanjim/nui.nvim",
-                --- The below dependencies are optional,
-                "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
-                "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-                "zbirenbaum/copilot.lua",      -- for providers='copilot'
-                {
-                    -- support for image pasting
-                    "HakonHarnes/img-clip.nvim",
-                    event = "VeryLazy",
-                    opts = {
-                        -- recommended settings
-                        default = {
-                            embed_image_as_base64 = false,
-                            prompt_for_file_name = false,
-                            drag_and_drop = {
-                                insert_mode = true,
-                            },
-                            -- required for Windows users
-                            use_absolute_path = true,
-                        },
-                    },
-                },
-                {
-                    -- Make sure to set this up properly if you have lazy=true
-                    'MeanderingProgrammer/render-markdown.nvim',
-                    opts = {
-                        file_types = { "markdown", "Avante" },
-                    },
-                    ft = { "markdown", "Avante" },
-                },
-            },
         }
     },
     rocks = { enabled = false }
